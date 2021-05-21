ab3a22cef8f5cc392621c7b93671ec31f7362f7f
Mayank Pruthi <mayankpruthi123@gmail.com>
2021-05-22 02:39:13 +0530

    Added deep populate for getting user profile and product reviews

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,20 @@
 const User = require("../models/user");
 const { cleanApiData } = require("../utils/helper");
 
+// get a single user
+// only accessible to the user signed in
 module.exports.getUser = async (req, res) => {
     try {
-        console.log(req.user);
-        const user = await User.findOne({ _id: req.user._id });
+        const user = await User.findOne({ _id: req.user._id }).populate({
+            path: "reviews",
+            model: "Review",
+            select: "rating review product",
+            populate: {
+                path: "product",
+                model: "Product",
+                select: "title",
+            },
+        });
         if (user) {
             const newUser = cleanApiData(user);
             return res.status(200).json({
@@ -20,6 +30,39 @@ module.exports.getUser = async (req, res) => {
     }
 };
 
+// get user by id
+// accessible to everyone who views the profile
+module.exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params.userId }).populate({
+            path: "reviews",
+            model: "Review",
+            select: "rating review product",
+            populate: {
+                path: "product",
+                model: "Product",
+                select: "title",
+            },
+        });
+        if (user) {
+            return res.status(200).json({
+                // user,
+                user: {
+                    name: user.name,
+                    reviews: user.reviews,
+                },
+                ok: true,
+            });
+        }
+    } catch (error) {
+        return res.status(422).json({
+            message: "User not found",
+            ok: false,
+        });
+    }
+};
+
+// only accessible by admin
 module.exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.find();
@@ -37,9 +80,11 @@ module.exports.getAllUsers = async (req, res) => {
     }
 };
 
+// users can update their account
 module.exports.updateUser = async (req, res) => {
     try {
         const { body } = req;
+        // so they cant update their role
         body.role = 0;
         const user = await User.findByIdAndUpdate(req.user._id, body, {
             new: true,
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { isLoggedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
-const { getAllUsers, getUser, updateUser } = require("../controllers/user");
+const { getAllUsers, getUser, updateUser, getUserById } = require("../controllers/user");
 const router = express.Router();
 
 router.get("/all", isLoggedIn, isAuthenticated, isAdmin, getAllUsers);
 router.get("/", isLoggedIn, isAuthenticated, getUser);
+router.get("/:userId", getUserById);
 router.put("/", isLoggedIn, isAuthenticated, updateUser)
 
 module.exports = router;
